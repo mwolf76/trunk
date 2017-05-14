c34e3a1d6444dff60b6d6ceb2922eea324d93094
Marco Pensallorto <marco.pensallorto@gmail.com>
2017-05-14 13:02:54 +0200

    Changed launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mvn clean package -DskipTests && \
-    java -jar -Dvertx.logger-delegate-factory-class-name="io.vertx.core.logging.SLF4JLogDelegateFactory" target/trunk-1.0-SNAPSHOT-fat.jar -conf conf/config.json run org.blackcat.trunk.verticles.TrunkVerticle
+java -jar -Dvertx.logger-delegate-factory-class-name="io.vertx.core.logging.SLF4JLogDelegateFactory" target/trunk-1.0-SNAPSHOT-fat.jar -conf conf/config.json run org.blackcat.trunk.verticles.TrunkVerticle | tee -a trunk.log
+
 
 
