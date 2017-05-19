5f4d523685fe1befa63e33e3d66edf40c1099f48
Marco Pensallorto <marco.pensallorto@gmail.com>
2017-05-19 20:19:32 +0200

    Minor changes; 4 instances deployed

--- src/main/java/org/blackcat/trunk/verticles/TrunkVerticle.java
+++ src/main/java/org/blackcat/trunk/verticles/TrunkVerticle.java
@@ -40,17 +40,17 @@ public class TrunkVerticle extends AbstractVerticle {
             ClassLoader cl = ClassLoader.getSystemClassLoader();
             URL[] urls = ((URLClassLoader)cl).getURLs();
             for(URL url: urls){
-                logger.info(url.getFile());
+                logger.debug(url.getFile());
             }
 
             /* retrieve configuration object from vert.x ctx */
             final Configuration configuration = new Configuration(vertx.getOrCreateContext().config());
             logger.info("Configuration: {}", configuration.toString());
 
-        /* configure Pebble template engine */
+            /* configure Pebble template engine */
             final PebbleTemplateEngine pebbleEngine = PebbleTemplateEngine.create(vertx);
 
-        /* connect to mongo data store */
+            /* connect to mongo data store */
             String connectionString = String.format("%s://%s:%s",
                     configuration.getDatabaseType(),
                     configuration.getDatabaseHost(),
@@ -61,13 +61,13 @@ public class TrunkVerticle extends AbstractVerticle {
                     .put("connection_string", connectionString)
                     .put("db_name", configuration.getDatabaseName());
 
-            MongoClient mongoClient = MongoClient.createNonShared(vertx, mongoConfig);
+            MongoClient mongoClient = MongoClient.createShared(vertx, mongoConfig);
             MongoDataStore mongoDataStore = new MongoDataStore(vertx, mongoClient, mongoConfig);
 
             Storage storage = new FileSystemStorage(vertx, logger,
                     Paths.get(configuration.getStorageRoot()));
 
-        /* configure request handler */
+            /* configure request handler */
             Handler<HttpServerRequest> handler =
                     new RequestHandler(vertx, pebbleEngine, logger, configuration, mongoDataStore, storage);
 
@@ -87,19 +87,24 @@ public class TrunkVerticle extends AbstractVerticle {
             vertx.createHttpServer(httpServerOptions)
                     .requestHandler(handler)
                     .listen(configuration.getHttpPort(), result -> {
-
                         if (result.succeeded()) {
                             logger.info("Ready to accept requests on port {}.",
                                     String.valueOf(configuration.getHttpPort()));
-
-                            startFuture.complete();
+                            future.complete();
                         } else {
-                            startFuture.fail(result.cause());
+                            future.fail(result.cause());
                         }
                     });
 
-            future.complete();
+
         }, res -> {
+            if (res.succeeded()) {
+                startFuture.complete();
+            }
+            else {
+                Throwable cause = res.cause();
+                startFuture.fail(cause);
+            }
         });
     }
 }
