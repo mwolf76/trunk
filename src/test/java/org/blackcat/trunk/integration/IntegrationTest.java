44e81ee26f5ccb3931c7f1c3cacf2b3142aeb51b
Marco Pensallorto <marco.pensallorto@gmail.com>
2018-02-07 09:30:54 +0100

    Tests now looking good.

--- src/test/java/org/blackcat/trunk/integration/IntegrationTest.java
+++ src/test/java/org/blackcat/trunk/integration/IntegrationTest.java
@@ -5,15 +5,13 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.blackcat.trunk.verticles.MainVerticle;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -34,17 +32,29 @@ import java.nio.file.Paths;
 public class IntegrationTest {
 
     static final String baseUrl = "https://localhost:8080";
+    static final String testUserEmail = "admin@myhost.co";
+    static final String rootPath = "/tmp/trunk";
 
-    static final Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
+    static final String loggedUserXPath = "/html/body/div/div[1]/div[1]/ul/li/div/span/a/strong";
+    static final String homePageHeaderXPath = "/html/body/div/div[1]/h1";
+    static final String logoutLinkXPath = "/html/body/div/div[2]/div[2]/div[2]/div/a/span";
+    static final String plusButtonXPath = "//*[@id=\"plus-button\"]";
+    static final String commitButtonXPath = "//*[@id=\"commit-additional-collection-resource\"]";
+    static final String firstCollectionXPath = "/html/body/div/div[1]/div[3]/ul/li";
+    static final String openLinkXPath = "/html/body/div/div[1]/a";
+    static final String usernameInputXPath = "//*[@id=\"username\"]";
+    static final String passwordInputXPath = "//*[@id=\"password\"]";
+    static final String loginButtonXPath = "//*[@id=\"kc-login\"]";
+    static final String loginFormXPath = "//*[@id=\"kc-form-login\"]";
+    static final String additionalCollectionNameInputXPath = "//*[@id=\"additional-collection-resource-input\"]";
 
-    static private WebDriver driver;
-    private Vertx vertx;
+    private WebDriver driver;
 
     @Rule
     public final RunTestOnContext rule = new RunTestOnContext(Vertx::vertx);
 
-    @BeforeClass
-    static public void init() {
+    @Before
+    public void init() {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         options.setAcceptInsecureCerts(true);
@@ -57,8 +67,8 @@ public class IntegrationTest {
         driver = new ChromeDriver(options);
     }
 
-    @AfterClass
-    static public void done(TestContext context) {
+    @After
+    public void done(TestContext context) {
         driver.close();
     }
 
@@ -67,7 +77,8 @@ public class IntegrationTest {
         Async async = context.async();
         deployForTesting(rule.vertx(), done -> {
             driver.get(baseUrl);
-            WebElement h1 = driver.findElement(By.xpath("/html/body/div/div[1]/h1"));
+
+            WebElement h1 = driver.findElement(By.xpath(homePageHeaderXPath));
 
             context.assertEquals("Trunk", h1.getText());
             async.complete();
@@ -78,15 +89,11 @@ public class IntegrationTest {
     public void privateIndex(TestContext context) {
         Async async = context.async();
         deployForTesting(rule.vertx(), done -> {
-            driver.get(baseUrl);
-
-            WebElement openLink = driver.findElement(By.xpath("/html/body/div/div[1]/a"));
-            openLink.click();
-
             adminLogin();
 
-            WebElement loggedUser = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/ul/li/div/span/a/strong"));
-            context.assertEquals("admin@myhost.co", loggedUser.getText());
+            WebElement loggedUser = driver.findElement(By.xpath(loggedUserXPath));
+            context.assertEquals(testUserEmail, loggedUser.getText());
+
             async.complete();
         });
     }
@@ -95,18 +102,14 @@ public class IntegrationTest {
     public void logout(TestContext context) {
         Async async = context.async();
         deployForTesting(rule.vertx(), done -> {
-            driver.get(baseUrl);
-
-            WebElement openLink = driver.findElement(By.xpath("/html/body/div/div[1]/a"));
-            openLink.click();
-
             adminLogin();
 
-            WebElement logoutLink = driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/div[2]/div/a/span"));
+            WebElement logoutLink = driver.findElement(By.xpath(logoutLinkXPath));
             logoutLink.click();
 
-            WebElement h1 = driver.findElement(By.xpath("/html/body/div/div[1]/h1"));
+            WebElement h1 = driver.findElement(By.xpath(homePageHeaderXPath));
             context.assertEquals("Trunk", h1.getText());
+
             async.complete();
         });
     }
@@ -115,35 +118,32 @@ public class IntegrationTest {
     public void createCollection(TestContext context) {
         Async async = context.async();
         deployForTesting(rule.vertx(), done -> {
-            driver.get(baseUrl);
-
-            WebElement openLink = driver.findElement(By.xpath("/html/body/div/div[1]/a"));
-            openLink.click();
             adminLogin();
 
-            WebElement plusButton = driver.findElement(By.xpath("//*[@id=\"plus-button\"]"));
+            WebElement plusButton = driver.findElement(By.xpath(plusButtonXPath));
             plusButton.click();
 
             new WebDriverWait(driver, 2)
                 .until(ExpectedConditions.visibilityOfElementLocated(
                     By.xpath("/html/body/div[1]/div[1]/div[5]/div")));
-            WebElement nameInput = driver.findElement(By.xpath("//*[@id=\"additional-collection-resource-input\"]"));
+            WebElement nameInput = driver.findElement(By.xpath(additionalCollectionNameInputXPath ));
             nameInput.sendKeys("whatever");
 
-            WebElement commitButton = driver.findElement(By.xpath("//*[@id=\"commit-additional-collection-resource\"]"));
+            WebElement commitButton = driver.findElement(By.xpath(commitButtonXPath));
             commitButton.click();
 
-            WebElement firstCollection = driver.findElement(By.xpath("/html/body/div/div[1]/div[3]/ul/li"));
+            WebElement firstCollection = driver.findElement(By.xpath(firstCollectionXPath));
 
             String text = firstCollection.getText().split("\n")[0];
             context.assertEquals("whatever", text);
+
             async.complete();
         });
     }
 
     private void deployForTesting(Vertx vertx, Handler<AsyncResult<String>> done) {
-        vertx.fileSystem().deleteRecursive("/tmp/trunk", true, _1-> {
-            vertx.fileSystem().mkdir("/tmp/trunk", _2 -> {
+        vertx.fileSystem().deleteRecursive(rootPath, true, _1-> {
+            vertx.fileSystem().mkdir(rootPath, _2 -> {
                 DeploymentOptions deploymentOptions = new DeploymentOptions();
                 deploymentOptions.setConfig(getConfiguration());
                 vertx.deployVerticle(MainVerticle.class.getName(), deploymentOptions, done);
@@ -152,23 +152,31 @@ public class IntegrationTest {
     }
 
     private void adminLogin() {
-        WebElement username = driver.findElement(By.id("username"));
+        driver.get(baseUrl);
+
+        WebElement openLink = driver.findElement(By.xpath(openLinkXPath));
+        openLink.click();
+
+        new WebDriverWait(driver, 2)
+            .until(ExpectedConditions.visibilityOfElementLocated(By.xpath(loginFormXPath)));
+
+        WebElement username = driver.findElement(By.xpath(usernameInputXPath));
         username.sendKeys("admin");
 
-        WebElement password = driver.findElement(By.id("password"));
+        WebElement password = driver.findElement(By.xpath(passwordInputXPath));
         password.sendKeys("admin");
 
-        WebElement loginButton = driver.findElement(By.id("kc-login"));
-        loginButton.click();
+        WebElement loginButton = driver.findElement(By.xpath(loginButtonXPath));
+        loginButton.submit();
     }
 
     private static JsonObject getConfiguration() {
-        byte[] bytes = new byte[0];
         try {
-            bytes = Files.readAllBytes(Paths.get("conf/config.json"));
+            byte[] bytes = Files.readAllBytes(Paths.get("conf/config.json"));
+            return new JsonObject(new String(bytes));
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return new JsonObject(new String(bytes));
     }
 }
